f5f3287bfad040d9c22661afe059367615975581
FreyaP <65002708+FreyaP@users.noreply.github.com>
2021-09-18 18:41:20 -0700

    Connecting to GutHub

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Volks 'N' Spokes</h1>
+        <h2>Mobile Bike Mechanic | Squamish, BC</h2>
+        <h3>Full site coming soon. In the meantime....</h3>
+        <p><a className="contact-link" href='tel:+16049358573'>Call Now - 604-935-8573</a></p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
