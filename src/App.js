830edf1079f3150d4400043b57808cb9d0a5b06a
FreyaP <65002708+FreyaP@users.noreply.github.com>
2021-12-16 15:03:41 -0800

    Update

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ function App() {
       <header className="App-header">
         <h1>Volks 'N' Spokes</h1>
         <h2>Mobile Bike Mechanic | Squamish, BC</h2>
-        <h3>Full site coming soon. In the meantime....</h3>
+        <h3>Winter Service Special - $130</h3>
         <p><a className="contact-link" href='tel:+16049358573'>Call Now - 604-935-8573</a></p>
+        <p>Full site coming soon.</p>
+        
         
       </header>
     </div>
